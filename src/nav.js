4a607db55bed2f887aa430f220f28549431e391b
AnthonyEspinozaCS <aespinoza.cs@gmail.com>
2023-07-03 16:01:52 -0700

    first commit

---
+++ src/nav.js
@@ -0,0 +1,21 @@
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
+
+function bio(number) {
+  const bio = document.getElementById(`bio-${number}`);
+  console.log(number);
+  bio.classList.toggle("hidden");
+  bio.classList.toggle("flex");
+}
