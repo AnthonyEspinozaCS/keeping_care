fde2cc42ecb1657a6eff26aa5ffd06ebae423838
AnthonyEspinozaCS <aespinoza.cs@gmail.com>
2023-07-04 13:11:28 -0700

    Added read more button to bios.

--- src/nav.js
+++ src/nav.js
@@ -13,9 +13,13 @@ navToggle.addEventListener("click", () => {
   }
 });
 
-function bio(number) {
-  const bio = document.getElementById(`bio-${number}`);
+function myFunction(number) {
+  const bio = document.getElementById(`more-${number}`);
+  const btnText = document.getElementById(`button-${number}`);
+  const dots = document.getElementById(`dots-${number}`);
   console.log(number);
   bio.classList.toggle("hidden");
-  bio.classList.toggle("flex");
+  dots.classList.toggle("hidden");
+  if (btnText.innerText === "Read More") btnText.innerText = "Read Less";
+  else btnText.innerText = "Read More";
 }
