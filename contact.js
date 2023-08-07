ed5ed44dd9763267f05b7944afec54527bf27878
AnthonyEspinozaCS <aespinoza.cs@gmail.com>
2023-08-07 14:57:52 -0700

    emailjs fixes.

--- contact.js
+++ contact.js
@@ -10,8 +10,8 @@ window.onload = function () {
     event.preventDefault();
     // generate a five digit number for the contact_number variable
     this.contact_number.value = (Math.random() * 100000) | 0;
-    this.client = "markdelaney@keepingcare.ca";
-    this.client_name = "Mark";
+    this.client.value = "markdelaney@keepingcare.ca";
+    this.client_name.value = "Mark";
     // these IDs from the previous steps
     emailjs.sendForm("contact_service", "contact_form", this).then(
       function () {
