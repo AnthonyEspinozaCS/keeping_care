e338020123c331f5c251ef686fa9ca8c1e2e339e
AnthonyEspinozaCS <aespinoza.cs@gmail.com>
2023-08-07 15:13:36 -0700

    Removed contact number.

--- contact.js
+++ contact.js
@@ -9,9 +9,10 @@ window.onload = function () {
   document.getElementById("contact-form").addEventListener("submit", function (event) {
     event.preventDefault();
     // generate a five digit number for the contact_number variable
-    this.contact_number.value = (Math.random() * 100000) | 0;
     this.client.value = "markdelaney@keepingcare.ca";
     this.client_name.value = "Mark";
+    console.log("hello");
+    setTimeout(3000);
     // these IDs from the previous steps
     emailjs.sendForm("contact_service", "contact_form", this).then(
       function (response) {
